83baded749e3d9797fcbc6c75ad8650ac38b984d
Esko Luontola <esko@luontola.fi>
2020-09-19 15:17:28 +0300

    uncodebar 6 session finished
    https://codebar.io/events/uncodebar-6

--- test/my_project/core_test.clj
+++ test/my_project/core_test.clj
@@ -2,6 +2,51 @@
   (:require [clojure.test :refer [deftest is testing]]
             [my-project.core :as core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def all-suits
+  [:heart :spade :club :diamond])
+(def all-ranks
+  [:ace :king :queen :jack 10 9 8 7 6 5 4 3 2])
+(def all-cards
+  (for [suit all-suits
+        rank all-ranks]
+    {:suit suit
+     :rank rank}))
+
+(deftest cards-test
+  (is (= {:suit :heart
+          :rank :ace}
+         (first all-cards)))
+  (is (= 52 (count all-cards))))
+
+(comment
+  ; https://clojuredocs.org/clojure.core/sort-by
+  ; https://clojuredocs.org/clojure.core/map
+  (map :rank [{:suit :spade, :rank 7}
+              {:suit :spade, :rank :queen}
+              {:suit :heart, :rank 4}])
+  (sort-by count ["aaa" "bb" "c"])
+  #(.indexOf all-ranks %))
+
+(defn get-highest-rank [ranks]
+  (->> ranks
+       (sort-by #(.indexOf all-ranks %))
+       (first)))
+
+(defn evaluate-hand [hand]
+  (let [rank (->> (map :rank hand)
+                  (get-highest-rank))]
+    {:category :high-card
+     :rank rank}))
+
+(deftest get-highest-rank-test
+  (is (= :king (get-highest-rank [7 :queen 4 3 :king]))))
+
+
+(deftest hands-test
+  (testing "high card"
+    (is (= {:category :high-card, :rank :king}
+           (evaluate-hand [{:suit :spade, :rank 7}
+                           {:suit :spade, :rank :queen}
+                           {:suit :heart, :rank 4}
+                           {:suit :heart, :rank 3}
+                           {:suit :diamond, :rank :king}])))))
