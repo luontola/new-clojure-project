c4a52b6503a93a125849ed9bcbb2e41fe701ade3
Esko Luontola <esko@luontola.fi>
2020-11-13 22:54:50 +0200

    Rock Paper Scissors

--- test/my_project/core_test.clj
+++ test/my_project/core_test.clj
@@ -2,6 +2,33 @@
   (:require [clojure.test :refer [deftest is testing]]
             [my-project.core :as core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def rules
+  (->> [[:paper :rock]
+        [:scissors :paper]
+        [:rock :scissors]]
+       (map (fn [[winner loser]]
+              {[winner loser] first
+               [loser winner] second
+               [winner winner] (constantly :draw)}))
+       (apply merge)))
+
+(defn winner [human-hand computer-hand]
+  (let [players [:human :computer]
+        hands [human-hand computer-hand]
+        select-winner (rules hands)]
+    (select-winner players)))
+
+
+(deftest rock-paper-scissors-test
+  (is (= :computer (winner :rock :paper)))
+  (is (= :human (winner :paper :rock)))
+
+  (is (= :computer (winner :paper :scissors)))
+  (is (= :human (winner :scissors :paper)))
+
+  (is (= :computer (winner :scissors :rock)))
+  (is (= :human (winner :rock :scissors)))
+
+  (is (= :draw (winner :rock :rock)))
+  (is (= :draw (winner :paper :paper)))
+  (is (= :draw (winner :scissors :scissors))))
