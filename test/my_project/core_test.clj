e8142e0819d50ad9ad3bfd82d120641db2187ddf
Esko Luontola <esko@luontola.fi>
2020-11-13 23:05:40 +0200

    Rock Paper Scissors Lizard Spock

--- test/my_project/core_test.clj
+++ test/my_project/core_test.clj
@@ -3,9 +3,16 @@
             [my-project.core :as core]))
 
 (def rules
-  (->> [[:paper :rock]
+  (->> [[:rock :scissors]
+        [:rock :lizard]
+        [:paper :rock]
+        [:paper :spock]
         [:scissors :paper]
-        [:rock :scissors]]
+        [:scissors :lizard]
+        [:lizard :paper]
+        [:lizard :spock]
+        [:spock :rock]
+        [:spock :scissors]]
        (map (fn [[winner loser]]
               {[winner loser] first
                [loser winner] second
@@ -15,20 +22,39 @@
 (defn winner [human-hand computer-hand]
   (let [players [:human :computer]
         hands [human-hand computer-hand]
-        select-winner (rules hands)]
+        select-winner (or (rules hands)
+                          (throw (IllegalArgumentException.
+                                  (str "Illegal hands: " hands))))]
     (select-winner players)))
 
 
 (deftest rock-paper-scissors-test
-  (is (= :computer (winner :rock :paper)))
-  (is (= :human (winner :paper :rock)))
+  (testing "human plays rock"
+    (is (= :draw (winner :rock :rock)))
+    (is (= :computer (winner :rock :paper)))
+    (is (= :human (winner :rock :scissors))))
 
-  (is (= :computer (winner :paper :scissors)))
-  (is (= :human (winner :scissors :paper)))
+  (testing "human plays paper"
+    (is (= :human (winner :paper :rock)))
+    (is (= :draw (winner :paper :paper)))
+    (is (= :computer (winner :paper :scissors))))
 
-  (is (= :computer (winner :scissors :rock)))
-  (is (= :human (winner :rock :scissors)))
+  (testing "human plays scissors"
+    (is (= :computer (winner :scissors :rock)))
+    (is (= :human (winner :scissors :paper)))
+    (is (= :draw (winner :scissors :scissors)))))
 
-  (is (= :draw (winner :rock :rock)))
-  (is (= :draw (winner :paper :paper)))
-  (is (= :draw (winner :scissors :scissors))))
+(deftest lizard-spock-test
+  (testing "human plays lizard"
+    (is (= :computer (winner :lizard :rock)))
+    (is (= :human (winner :lizard :paper)))
+    (is (= :computer (winner :lizard :scissors)))
+    (is (= :draw (winner :lizard :lizard)))
+    (is (= :human (winner :lizard :spock))))
+
+  (testing "human plays spock"
+    (is (= :human (winner :spock :rock)))
+    (is (= :computer (winner :spock :paper)))
+    (is (= :human (winner :spock :scissors)))
+    (is (= :computer (winner :spock :lizard)))
+    (is (= :draw (winner :spock :spock)))))
