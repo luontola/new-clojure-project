ece0ac8221125b53423c152fb5354c9bfa5b9fe1
Esko Luontola <esko@luontola.fi>
2023-01-11 18:26:55 +0200

    Refactor: lights mask

--- src/berlin_clock.clj
+++ src/berlin_clock.clj
@@ -1,10 +1,14 @@
 (ns berlin-clock
   (:import (java.time LocalTime)))
 
+(def lights-off (repeat \O))
+
+(defn lights [colors n]
+  (let [lights-count (count colors)
+        lights-on (take n colors)]
+    (->> (concat lights-on lights-off)
+         (take lights-count)
+         (apply str))))
+
 (defn time->1-minutes [^LocalTime time]
-  (case (mod (.getMinute time) 5)
-    0 "OOOO"
-    1 "YOOO"
-    2 "YYOO"
-    3 "YYYO"
-    4 "YYYY"))
+  (lights "YYYY" (mod (.getMinute time) 5)))
