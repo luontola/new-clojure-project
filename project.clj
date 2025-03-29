7af5475ec493dfbb7f21d76d2b89f1028e10d8f8
Esko Luontola <esko@luontola.fi>
2025-03-29 10:09:05 +0200

    Minimal project.clj

--- project.clj
+++ project.clj
@@ -1,13 +1,7 @@
 (defproject new-clojure-project "0.1.0-SNAPSHOT"
-
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/tools.logging "1.3.0"]]
-  :pedantic? :abort
-  :global-vars {*warn-on-reflection* true}
-
   :main ^:skip-aot kata
   :target-path "target/%s"
-  :javac-options ["--release" "11"]
-
   :profiles {:uberjar {:aot :all
                        :uberjar-name "kata.jar"}})
